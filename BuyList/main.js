83e7816346097a68eff3eae335112e5f9a47949a
Igor Shudra <shudra.ia@gmail.com>
2017-10-10 00:45:19 +0300

    Some JS
    - rows now delete
    - buttons +/- work as intended

--- BuyList/main.js
+++ BuyList/main.js
@@ -1,3 +1,56 @@
 $(function(){
-    console.log("Mat'yt kutjyhnrhryryjj");
+
+    var $list = $(".bl-list");
+    var ONE_ROW_TEMPLATE = $(".row-template").html();
+
+    var $bl_bought = $(".bl-bought");
+    var $left = $bl_bought.find(".bl-title-left");
+    var $bought = $bl_bought.find(".bl-title-bought");
+    var ONE_BOUGHT_TEMPLATE = $(".bought-template").html();
+
+    function addItem(title) {
+        var $node = $(ONE_ROW_TEMPLATE);
+        var $nodeRight = $(ONE_BOUGHT_TEMPLATE);
+
+        var quantity = 1;
+
+        var $quantity_label = $node.find(".bl-label");
+        $quantity_label.text(quantity);
+
+        var $quantity_label_right = $nodeRight.find(".bl-left-number");
+        $quantity_label_right.text(quantity);
+
+        $node.find(".bl-product").text(title);
+        $nodeRight.find(".bl-left-text").text(title);
+
+        $node.find(".bl-plus").click(function() {
+            quantity++;
+            $quantity_label.text(quantity);
+            $quantity_label_right.text(quantity);
+        });
+
+        $node.find(".bl-minus").click(function() {
+            if (quantity > 1) {
+                quantity--;
+                $quantity_label.text(quantity);
+                $quantity_label_right.text(quantity);
+            }
+        });
+
+        $node.find(".bl-remove-button").click(function(){
+           $node.remove();
+           $nodeRight.remove();
+        });
+
+        $list.append($node);
+        $left.append($nodeRight);
+    }
+
+
+    addItem("Помідори");
+    addItem("Огірки");
+    addItem("Маргальцовка");
+    addItem("Яблочкі");
+
+    console.log("Яблочкі!!!");
 });
